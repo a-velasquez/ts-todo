a7c1923b26f71283e106cbba80d07c2c0f12710b
Alex Velazquez <avelasquez075@gmail.com>
2022-03-29 13:51:46 -0700

    add handleChange() to task input

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {ChangeEvent, FC, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,12 +6,14 @@ const App: FC = () => {
   const [task, setTask] = useState<string>('')
   const [deadline, setDeadline] = useState<number>(0)
   const [todo, setTodoList] = useState([])
-  
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {}
+
   return (
     <div className="App">
       <div className='header'>
         <div className='inputContainer'>
-          <input type='text' placeholder='Task...'></input>
+          <input type='text' placeholder='Task...' onChange={handleChange}></input>
           <input type='number' placeholder='Deadline (in days)'></input>
         </div>
           <button>Add task</button>
