c79996bdb7459396a3d82d79ee808e0537f930ec
Alex Velazquez <avelasquez075@gmail.com>
2022-03-29 14:26:36 -0700

    add value to inputs, task interface, addTask()

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
-import React, {ChangeEvent, FC, useState} from 'react';
-import './App.css';
+import React, { ChangeEvent, FC, useState } from "react"
+import "./App.css"
+import { ITask } from './Interfaces'
 
 const App: FC = () => {
-  const [task, setTask] = useState<string>('')
-  const [deadline, setDeadline] = useState<number>(0)
-  const [todo, setTodoList] = useState([])
+	const [task, setTask] = useState<string>("")
+	const [deadline, setDeadline] = useState<number>(0)
+	const [todoList, setTodoList] = useState<ITask[]>([])
 
-  // return type void because not returning anything
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === 'task') {
-      setTask(event.target.value)
-    } else {
-      setDeadline(Number(event.target.value))
-    }
-  }
+	// return type void because not returning anything
 
-  return (
-    <div className="App">
-      <div className='header'>
-        <div className='inputContainer'>
-          <input type='text' placeholder='Task...' name='task' onChange={handleChange}></input>
-          <input type='number' placeholder='Deadline (in days)' name='deadline'></input>
-        </div>
-          <button>Add task</button>
-      </div>
-      <div className='todoList'></div>
-    </div>
-  );
+	const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+		if (event.target.name === "task") {
+			setTask(event.target.value)
+		} else {
+			setDeadline(Number(event.target.value))
+		}
+	}
+
+	const addTask = (): void => {
+		const newTask = { taskName: task, deadline: deadline }
+    setTodoList([...todoList, newTask])
+    setTask('')
+    setDeadline(0)
+	}
+
+	return (
+		<div className="App">
+			<div className="header">
+				<div className="inputContainer">
+					<input
+						type="text"
+						placeholder="Task..."
+						name="task"
+            value={task}
+						onChange={handleChange}
+					></input>
+					<input
+						type="number"
+						placeholder="Deadline (in days)"
+						name="deadline"
+            value={deadline}
+					></input>
+				</div>
+				<button onClick={addTask}>Add task</button>
+			</div>
+			<div className="todoList"></div>
+		</div>
+	)
 }
 
-export default App;
+export default App
